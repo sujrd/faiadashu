7d78f743284f12efd85d4ca0820a2d9165357750
Hanny Prastya Hariyadi <p.hanny@allm.inc>
2023-08-18 19:26:00 +0900

    Use QuestionnaireStepperPageView

--- lib/questionnaires/view/src/questionnaire_stepper.dart
+++ lib/questionnaires/view/src/questionnaire_stepper.dart
@@ -1,5 +1,6 @@
 import 'package:faiadashu/l10n/l10n.dart';
 import 'package:faiadashu/questionnaires/questionnaires.dart';
+import 'package:faiadashu/questionnaires/view/src/questionnaire_stepper_page_view.dart';
 import 'package:faiadashu/resource_provider/resource_provider.dart';
 import 'package:fhir/r4.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +12,7 @@ class QuestionnaireStepper extends StatefulWidget {
   final LaunchContext launchContext;
   final QuestionnairePageScaffoldBuilder scaffoldBuilder;
   final QuestionnaireModelDefaults questionnaireModelDefaults;
-  final PageController? pageController;
+  final QuestionnaireStepperPageViewController? controller;
 
   final void Function(QuestionnaireResponseModel?)?
       onQuestionnaireResponseChanged;
@@ -29,7 +30,7 @@ class QuestionnaireStepper extends StatefulWidget {
     this.onPageChanged,
     this.onLastPageUpdated,
     this.onVisibleItemUpdated,
-    this.pageController,
+    this.controller,
     Key? key,
   }) : super(key: key);
 
@@ -38,38 +39,27 @@ class QuestionnaireStepper extends StatefulWidget {
 }
 
 class QuestionnaireStepperState extends State<QuestionnaireStepper> {
-  BuildContext? _itemBuilderContext;
   QuestionnaireResponseModel? _questionnaireResponseModel;
+  QuestionnaireStepperPageViewController? _controller;
   bool _isLoaded = false;
   bool _lastPageState = false;
-  int? _currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? QuestionnaireStepperPageViewController();
+  }
 
   /// Notifies listeners when there are changes in the questionnaire response.
   void _handleChangedQuestionnaireResponse() {
     widget.onQuestionnaireResponseChanged?.call(_questionnaireResponseModel);
-    if (_currentIndex != null) {
-      _checkAndUpdatePageState(_currentIndex!);
-    }
-  }
-
-  /// Determines if the given index corresponds to the last page.
-  bool _hasReachedLastPage(int index) {
-    if (_itemBuilderContext == null) {
-      return false;
-    }
-    /// By checking the next item from the item builder, it verifies whether we're on the last page.
-    /// If there's no item for the next index, then we've reached the last page.
-    return QuestionnaireTheme.of(_itemBuilderContext!).stepperPageItemBuilder(
-          _itemBuilderContext!,
-          QuestionnaireResponseFiller.of(_itemBuilderContext!),
-          index + 1,
-        ) ==
-        null;
+    // The last page can change based on user responses.
+    _checkAndUpdatePageState();
   }
 
   /// Checks the current page status and updates the last page state accordingly.
-  void _checkAndUpdatePageState(int index) {
-    final currentState = _hasReachedLastPage(index);
+  void _checkAndUpdatePageState({bool? state}) {
+    final currentState = state ?? _controller?.hasReachedLastPage() ?? false;
     /// If the current state differs from the last known page state, listeners are notified.
     if (currentState != _lastPageState) {
       widget.onLastPageUpdated?.call(currentState);
@@ -77,35 +67,8 @@ class QuestionnaireStepperState extends State<QuestionnaireStepper> {
     }
   }
 
-  /// Updates the currently visible item based on the provided index.
-  ///
-  /// This ensures that the parent context knows which item is visible and can perform any
-  /// necessary actions or updates related to that item.
-  void _updateVisibleItem(int index) {
-    if (_itemBuilderContext == null) {
-      return;
-    }
-    final responseFiller = QuestionnaireResponseFiller.of(_itemBuilderContext!);
-
-    widget.onVisibleItemUpdated?.call(responseFiller.visibleItemFillerAt(index));
-  }
-
-  /// Manages tasks related to page index changes.
-  ///
-  /// This function is called when the user navigates to a different page.
-  /// It updates the visible item, checks the state of the last page, notifies listeners
-  /// of the change, and updates the current index.
-  void _handleChangedPage(int index) {
-    _updateVisibleItem(index);
-    _checkAndUpdatePageState(index);
-    widget.onPageChanged?.call(index);
-    _currentIndex = index;
-  }
-
   @override
   Widget build(BuildContext context) {
-    final controller = widget.pageController ?? PageController();
-
     return QuestionnaireResponseFiller(
       locale: widget.locale ?? Localizations.localeOf(context),
       fhirResourceProvider: widget.fhirResourceProvider,
@@ -118,38 +81,22 @@ class QuestionnaireStepperState extends State<QuestionnaireStepper> {
           child: Column(
             children: [
               Expanded(
-                child: PageView.builder(
-                  /// [PageView.scrollDirection] defaults to [Axis.horizontal].
-                  /// Use [Axis.vertical] to scroll vertically.
-                  controller: controller,
-                  onPageChanged: _handleChangedPage,
-                  itemBuilder: (BuildContext context, int index) {
-                    // Store the current context of the item builder.
-                    // This is done so that we can access this context outside the builder.
-                    _itemBuilderContext = context;
-                    // Update the state or properties associated with the currently visible item.
-                    _updateVisibleItem(index);
-                    return QuestionnaireTheme.of(context).stepperPageItemBuilder(
-                      context,
-                      QuestionnaireResponseFiller.of(context),
-                      index,
-                    );
+                child: QuestionnaireStepperPageView(
+                  controller: _controller,
+                  onPageChanged: widget.onPageChanged,
+                  onVisibleItemUpdated: widget.onVisibleItemUpdated,
+                  onLastPageUpdated: (state) {
+                    _checkAndUpdatePageState(state: state);
                   },
-                  physics: widget.pageController != null
-                      ? const NeverScrollableScrollPhysics()
-                      : null,
                 ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  if (widget.pageController == null)
+                  if (widget.controller == null)
                     IconButton(
                       icon: const Icon(Icons.arrow_back),
-                      onPressed: () => controller.previousPage(
-                        curve: Curves.easeIn,
-                        duration: const Duration(milliseconds: 250),
-                      ),
+                      onPressed: () => widget.controller?.previousPage(),
                     ),
                   Expanded(
                     child: Column(
@@ -182,13 +129,10 @@ class QuestionnaireStepperState extends State<QuestionnaireStepper> {
                       ],
                     ),
                   ),
-                  if (widget.pageController == null)
+                  if (widget.controller == null)
                     IconButton(
                       icon: const Icon(Icons.arrow_forward),
-                      onPressed: () => controller.nextPage(
-                        curve: Curves.easeIn,
-                        duration: const Duration(milliseconds: 250),
-                      ),
+                      onPressed: () => widget.controller?.nextPage(),
                     ),
                 ],
               ),
