98b2524af9b986443fe7b93d7033cc3de082e406
Hanny Prastya Hariyadi <p.hanny@allm.inc>
2023-10-04 17:08:04 +0900

    Add CommonValidationError

--- lib/questionnaires/model/src/validation_errors/common_validation_error.dart
+++ lib/questionnaires/model/src/validation_errors/common_validation_error.dart
@@ -1,9 +1,9 @@
 import 'package:faiadashu/l10n/src/fdash_localizations.g.dart';
 import 'package:faiadashu/questionnaires/model/src/validation_errors/validation_error.dart';
 
-class CustomValidationError extends ValidationError {
+class CommonValidationError extends ValidationError {
   final String? message;
-  const CustomValidationError(String nodeUid, this.message) : super(nodeUid);
+  const CommonValidationError(String nodeUid, this.message) : super(nodeUid);
 
   @override
   String? getMessage(FDashLocalizations localizations) {
